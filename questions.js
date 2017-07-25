0e87c41c2fb24db33de9a72a7bd6fc592e715d5c
mashelw <Mashelwaiz@gmail.com>
2017-07-25 12:03:47 -0700

    added changes

---
+++ questions.js
@@ -0,0 +1,23 @@
+exports.questions = [
+	{
+		full: 'Steve Carell played Michael Scott in the series The Office.',
+		cloze: 'Steve Carell'
+	},
+	{
+		full: 'Producers wanted John Krasinski to originally play Dwight.',
+		cloze: 'John Krasinski'
+	},
+	{
+		full: "In S1E2 Diversity Day: Michael imitate comedian Chris Rock stand up routine",
+		cloze: 'Chris Rock'
+	},
+	{
+		full: "In S1E4 'The Alliance':  Michael donate $25 to Oscar's nephew's charity, not realizing it is a walk-a-thon and the amount is per mile",
+		cloze: '$25'
+	},
+	{
+		full: "In S2E10 'Christmas Party': Pam ends up with the Video iPod at the end of the episode.",
+		cloze: 'Pam'
+	}
+]
+	
